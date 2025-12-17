ece014d718d3a3f730c8f74db536b7a287c03f22
Jeffersson <barrosjefferson407@gmail.com>
2025-12-16 21:26:29 -0300

    add app services

--- services/models.py
+++ services/models.py
@@ -1,3 +1,29 @@
+import shortuuid
+
+from enterprise.models import Enterprise
+
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        default=shortuuid.uuid,
+        max_length=22,
+        editable=False,
+        unique=True
+    )
+    service_name = models.CharField(max_length=255)
+    image_service = models.ImageField(upload_to='services/images/', null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.DurationField()
+    description = models.TextField(null=True, blank=True)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE, related_name='services')
+
+    class Meta:
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+        ordering = ['service_name']
+
+    def __str__(self):
+        return self.service_name
