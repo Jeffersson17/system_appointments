b21c2450292a1e1e40bb15c990b59be963178ab2
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2026-02-24 17:19:51 -0300

    fix docker (#4)

--- django/system_appointments/settings.py
+++ django/system_appointments/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,12 +77,12 @@ WSGI_APPLICATION = "system_appointments.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'HOST': os.getenv('POSTGRES_HOST', "postgres"),
+        'PORT': os.getenv('POSTGRES_PORT', "5432"),
     }
 }
 
