ef0d14c7173a5eb579fddee7423385f1f30c4475
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-03-25 19:10:01 -0300

    code quality

--- django/clients/serializers.py
+++ django/clients/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from clients.models import Client
-from enterprise.serializers import EnterpriseSerializer
 from user.models import User
 from user.serializers import UserSerializer
 
