35cbba56007ae0cbd4e776431d217d6fac46524f
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2026-03-25 19:10:39 -0300

    create user automatically (#9)
    * create user automatically
    
    * code quality
    
    * code quality

--- django/clients/serializers.py
+++ django/clients/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
 
 from clients.models import Client
-from enterprise.serializers import EnterpriseSerializer
+from user.models import User
 from user.serializers import UserSerializer
 
 
 class ClientSerializer(serializers.ModelSerializer):
-    enterprise = EnterpriseSerializer(read_only=True)
     user = UserSerializer(read_only=True)
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Client
-        fields = ["id", "first_name", "last_name", "phone_number", "enterprise", "user"]
+        fields = ["id", "first_name", "last_name", "phone_number", "enterprise", "user", "email", "password"]
+
+    def create(self, validated_data):
+        email = validated_data.pop("email")
+        password = validated_data.pop("password")
+        user = User.objects.create_user(
+            name=f"{validated_data['first_name']} {validated_data['last_name']}", email=email, password=password
+        )
+        client = Client.objects.create(user=user, **validated_data)
+        return client
--- django/enterprise/serializers.py
+++ django/enterprise/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
 
 from enterprise.models import Enterprise
+from user.models import User
+from user.serializers import UserSerializer
 
 
 class EnterpriseSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Enterprise
-        fields = "__all__"
+        fields = ["id", "company_name", "owner_name", "logotipo", "user", "password", "email"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        email = validated_data.pop("email")
+        user = User.objects.create_user(
+            name=validated_data["owner_name"], email=email, password=password, role="ENTERPRISE"
+        )
+        enterprise = Enterprise.objects.create(user=user, **validated_data)
+        return enterprise
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request = self.context.get("request")
+        if request and request.method in ["POST"]:
+            fields["email"].required = True
+            fields["password"].required = True
+        else:
+            fields.pop("email", None)
+            fields.pop("password", None)
+        return fields
