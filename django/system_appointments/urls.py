70725197def0b81d53a49739ea31f44b8dfbabfb
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-03-26 20:06:27 -0300

    add urls to the token simplejwt

--- django/system_appointments/urls.py
+++ django/system_appointments/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for system_appointments project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -23,6 +6,11 @@ from clients.views import ClientViewSet
 from enterprise.views import EnterpriseViewSet
 from services.views import ServiceViewSet
 from user.views import UserViewSet
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -33,4 +21,6 @@ router.register(r"clients", ClientViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
