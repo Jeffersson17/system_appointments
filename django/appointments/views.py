51b1bce676d7cdbc406dbd5cffd45bb4a07fdee1
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-03-30 16:34:48 -0300

    IsAutheticated in views

--- django/appointments/views.py
+++ django/appointments/views.py
@@ -1,16 +1,16 @@
 from appointments.models import Appointment
 from rest_framework import viewsets, generics
 from appointments.serializers import AppointmentSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
 
 class AppointmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Appointment.objects.all()
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = AppointmentSerializer
