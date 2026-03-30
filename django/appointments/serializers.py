c035a81a0463d13e58a8ad3f407eabbf9f0347ac
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-03-30 16:34:30 -0300

    filter for user

--- django/appointments/serializers.py
+++ django/appointments/serializers.py
@@ -7,3 +7,16 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
+
+    def validate(self, data):
+        user = self.context['request'].user
+        if user.role == "CLIENT":
+            client = user.client
+            queryset = Appointment.objects.filter(client=client, status="SCHEDULED").exists()
+            if self.instance:
+                queryset = queryset.exclude(id=self.instance.id)
+            if queryset.exists():
+                raise serializers.ValidationError(
+                    {"detail": "Você já possui um agendamento ativo."}
+                )
+        return data
